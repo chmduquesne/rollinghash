55c4ad2eafd2d5e1138dab6e6990ba4060972eba
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-04-22 15:31:52 +0200

    Adding the Roller interface (more idiomatic than just implementing Roll)

--- rollinghash.go
+++ rollinghash.go
@@ -39,41 +39,36 @@ package rollinghash
 
 import "hash"
 
-// RollingHash is the common interface implemented by all rolling
-// checksums. A RollingHash can be updated byte by byte, by specifying
-// which byte enters the window.
+type Roller interface {
+	// Roll updates the hash of a rolling window from the entering byte.
+	// A copy of the window is internally kept from the last Write().
+	// Roll updates this copy and the internal state of the checksum, and
+	// ideally (at least this is true for adler32), determines the new
+	// hash just from the current state, the entering byte, and the
+	// leaving byte.
+	Roll(b byte) error
+}
+
+// rollinghash.Hash extends hash.Hash by adding the method Roll. A
+// rollinghash.Hash can be updated byte by byte, by specifying which byte
+// enters the window.
 type Hash interface {
 	hash.Hash
-
-	// Roll updates the hash of a rolling window from the entering byte.
-	// A copy of the window is internally kept from the last Write().
-	// Roll updates this copy and the internal state of the checksum, and
-	// ideally (at least this is true for adler32), determines the new
-	// hash just from the current state, the entering byte, and the
-	// leaving byte.
-	Roll(b byte) error
+	Roller
 }
 
+// rollinghash.Hash32 extends hash.Hash by adding the method Roll. A
+// rollinghash.Hash32 can be updated byte by byte, by specifying which
+// byte enters the window.
 type Hash32 interface {
 	hash.Hash32
-
-	// Roll updates the hash of a rolling window from the entering byte.
-	// A copy of the window is internally kept from the last Write().
-	// Roll updates this copy and the internal state of the checksum, and
-	// ideally (at least this is true for adler32), determines the new
-	// hash just from the current state, the entering byte, and the
-	// leaving byte.
-	Roll(b byte) error
+	Roller
 }
 
+// rollinghash.Hash64 extends hash.Hash by adding the method Roll. A
+// rollinghash.Hash64 can be updated byte by byte, by specifying which
+// byte enters the window.
 type Hash64 interface {
 	hash.Hash64
-
-	// Roll updates the hash of a rolling window from the entering byte.
-	// A copy of the window is internally kept from the last Write().
-	// Roll updates this copy and the internal state of the checksum, and
-	// ideally (at least this is true for adler32), determines the new
-	// hash just from the current state, the entering byte, and the
-	// leaving byte.
-	Roll(b byte) error
+	Roller
 }
