1a0450ec3a03a1f65021e3b98abad0b6a7d0736f
Christophe-Marie Duquesne <chmd@chmd.fr>
2015-05-08 12:38:45 +0200

    Making the function actually tested

--- adler32/doc_test.go
+++ adler32/doc_test.go
@@ -1,6 +1,7 @@
 package adler32_test
 
 import (
+	"fmt"
 	"hash/adler32"
 	"log"
 
@@ -32,10 +33,41 @@ func Example() {
 			log.Fatal(err)
 		}
 
+		fmt.Printf("%v: checksum %x\n", string(s[i-n+1:i+1]), rolling.Sum32())
+
 		if vanilla.Sum32() != rolling.Sum32() {
 			log.Fatalf("%v: expected %x, got %x",
 				s[i-n+1:i+1], vanilla.Sum32(), rolling.Sum32())
 		}
-
 	}
+
+	// Output:
+	// he quick brown f: checksum 31e905d9
+	// e quick brown fo: checksum 314805e0
+	//  quick brown fox: checksum 30ea05f3
+	// quick brown fox : checksum 34dc05f3
+	// uick brown fox j: checksum 33b705ec
+	// ick brown fox ju: checksum 325205ec
+	// ck brown fox jum: checksum 31b105f0
+	// k brown fox jump: checksum 317d05fd
+	//  brown fox jumps: checksum 30d10605
+	// brown fox jumps : checksum 34d50605
+	// rown fox jumps o: checksum 34c60612
+	// own fox jumps ov: checksum 33bb0616
+	// wn fox jumps ove: checksum 32d6060c
+	// n fox jumps over: checksum 316c0607
+	//  fox jumps over : checksum 304405b9
+	// fox jumps over t: checksum 3450060d
+	// ox jumps over th: checksum 33fe060f
+	// x jumps over the: checksum 33120605
+	//  jumps over the : checksum 313e05ad
+	// jumps over the l: checksum 353605f9
+	// umps over the la: checksum 348505f0
+	// mps over the laz: checksum 332905f5
+	// ps over the lazy: checksum 32590601
+	// s over the lazy : checksum 310905b1
+	//  over the lazy d: checksum 2f7a05a2
+	// over the lazy do: checksum 336a05f1
+	// ver the lazy dog: checksum 326205e9
+
 }
