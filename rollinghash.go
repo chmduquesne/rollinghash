780fd5ad9de594fb904ebc091dca170c4d191d66
Christophe-Marie Duquesne <chmd@chmd.fr>
2017-11-25 12:18:00 +0100

    Updating the documentation (readability in godoc)

--- rollinghash.go
+++ rollinghash.go
@@ -11,13 +11,15 @@ import "hash"
 // new Hash.
 const DefaultWindowCap = 64
 
+// A Roller is a type that has the method Roll. Roll updates the hash of a
+// rolling window from just the entering byte. You MUST call Write()
+// BEFORE using this method and provide it with an initial window of size
+// at least 1 byte. You can then call this method for every new byte
+// entering the window. The byte leaving the window is automatically
+// computed from a copy of the window internally kept in the checksum.
+// This window is updated along with the internal state of the checksum
+// every time Roll() is called.
 type Roller interface {
-	// Roll updates the hash of a rolling window from just the entering
-	// byte. You MUST call Write() and provide it with an initial window
-	// of size at least 1 byte before using this method. You can then call
-	// this method for every new byte entering the window. A copy of the
-	// window is internally kept, and is updated along with the internal
-	// state of the checksum every time Roll() is called.
 	Roll(b byte)
 }
 
